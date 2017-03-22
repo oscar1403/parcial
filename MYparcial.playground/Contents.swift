55c92706a3af5f63cd546533ff364b5256284618
Oscar Londoño <oscar@MBPro-Oscar-Londono.local>
2017-03-22 07:59:24 -0500

    Inicio del proyecto rama principal

---
+++ MYparcial.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//programa para calcular el promedio de un alumno  y sus cinco materias principales  al final debe tener una variable que diga true si el promedio del alumno es 100
+
+var  nota1 = 100
+var nota3 = 100
+var nota4 = 100
+var nota5 = 100
+
+
+
+
+
